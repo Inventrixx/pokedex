9022bd41c65600764f7776d62e074f3dc0ed3ec7
Inventrixx <inveniahadouken@gmail.com>
2020-06-02 19:36:12 -0300

    (chore): setting up server & npm express

---
+++ back/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.listen(8080, function() {
+  console.log("Listening at localhost:8080");
+});
