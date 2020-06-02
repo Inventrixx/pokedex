a12a2412dc2d2082b81e92189779dde3b5d34d59
Inventrixx <inveniahadouken@gmail.com>
2020-06-02 19:53:59 -0300

    server: plugged in routes in the server

--- back/server.js
+++ back/server.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const routes = require("./routes");
+
+routes(app);
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  //allow CROS Origin from my localhost domain to my API.
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
