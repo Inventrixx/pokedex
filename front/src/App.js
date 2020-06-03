bc61dbfc1f083848f160fbde9e4a046c462518ff
Inventrixx <inveniahadouken@gmail.com>
2020-06-03 11:52:06 -0300

    (App): created pokedex component

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import SearchBar from "./SearchBar/SearchBar";
-import PokemonList from "./PokemonList/PokemonList";
+import React from "react";
+import Pokedex from "./components/Pokedex";
 import "./App.css";
 
 const App = () => {
-  const [myPokemonFetched, setMyPokemonFetched] = useState([]);
-
-  useEffect(() => {
-    const myFetchedPokemon = async () => {
-      try {
-        let myFetchedPokemon = await axios.get(`http://localhost:8080/api/`);
-        setMyPokemonFetched(myFetchedPokemon.data.results);
-      } catch (e) {
-        console.warn(e);
-      }
-    };
-    myFetchedPokemon();
-  }, []);
-
-  return (
-    <section className="pokedex-main-section">
-      <SearchBar />
-      <PokemonList myPokemonFetched={myPokemonFetched} />
-    </section>
-  );
+  return <Pokedex />;
 };
 
 export default App;
