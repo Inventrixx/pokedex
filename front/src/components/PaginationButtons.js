82d5bddb66c64641911d84ec0cb9088a5610d71c
Inventrixx <inveniahadouken@gmail.com>
2020-06-04 18:54:58 -0300

    (paginationButton): added component for pagination

---
+++ front/src/components/PaginationButtons.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const PaginationButtons = ({ addPage, setAddPage }) => {
+  let searchPerPage = 5;
+  return (
+    <div className="pokedex-pagination">
+      <button
+        className="button button-prev"
+        disabled={addPage === 0}
+        onClick={() => setAddPage(addPage - searchPerPage)}
+      >
+        Prev!
+      </button>
+      <button
+        className="button button-next"
+        onClick={() => setAddPage(addPage + searchPerPage)}
+      >
+        Next!
+      </button>
+    </div>
+  );
+};
+
+export default PaginationButtons;
