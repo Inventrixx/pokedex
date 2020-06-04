1a6dea8501c3e4ef0bcb1e20e7c8f1c8330ceb3c
Inventrixx <inveniahadouken@gmail.com>
2020-06-04 18:57:58 -0300

    (pokemonList): finished structure, added loader when waiting for promises

--- front/src/components/PokemonList.js
+++ front/src/components/PokemonList.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "react-loader-spinner";
+import { getUniquePokemon } from "../calls/getUniquePokemon";
 
-const PokemonList = ({
-  myPokemonSearched,
-  myPokemonFetched,
-  filteredPokemons
-}) => {
-  const noValue = myPokemonFetched && myPokemonSearched.length === 0;
+const PokemonList = ({ pokeName, page }) => {
+  const [myPokeInfo, setMyPokeInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    getUniquePokemon(pokeName)
+      .then(response => {
+        setMyPokeInfo(response);
+      })
+      .catch(e => console.warn(e))
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      });
+  }, [page, pokeName]);
+
+  let typeTransformed =
+    myPokeInfo.type &&
+    myPokeInfo.type.map((type, i) => {
+      return <span key={i}> {type} </span>;
+    });
   return (
-    myPokemonFetched &&
-    (noValue ? (
-      <div>
-        {myPokemonFetched.slice(0, 3).map(pokemon => (
-          <ul>
-            <li>{pokemon.name}</li>
-          </ul>
-        ))}
-      </div>
-    ) : (
-      <div>
-        {filteredPokemons.map(pokemon => {
-          return <div>{pokemon.name}</div>;
-        })}
-      </div>
-    ))
+    <section className="pokemon-pokeList">
+      {loading ? (
+        <Loader type="ThreeDots" color="#00BFFF" height={100} width={100} />
+      ) : (
+        <div className="pokemon-pokeList-container">
+          <div className="pokemon-pokeList-container-header">
+            <span># {myPokeInfo.id} </span>
+            <span>{pokeName}</span>
+          </div>
+          <div>
+            <div className="pokedex-pokemon-information">
+              <div className="pokedex-pokemon-information-img">
+                <img src={myPokeInfo.img} />
+              </div>
+              <ul className="pokedex-pokemon-information-details">
+                <li>type: {typeTransformed}</li>
+                <li>weight: {myPokeInfo.weight} hg</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
   );
 };
 
